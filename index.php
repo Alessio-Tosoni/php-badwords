510fde7e6aae756925aacbac579de4294a1a9770
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-12-18 16:54:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php badwords</title>
+    <?php $messaggio = "Hello World"?>
+</head>
+<body>
+    <h1><?php echo "$messaggio"?></h1>
+</body>
+</html>
